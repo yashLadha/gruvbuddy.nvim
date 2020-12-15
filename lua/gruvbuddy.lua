7aa8ced5573f81b09afdc0b08371274e3af817d5
Yash Ladha <yashladhapankajladha123@gmail.com>
2020-12-15 20:14:35 +0530

    added group for tabline

--- lua/gruvbuddy.lua
+++ lua/gruvbuddy.lua
@@ -4,7 +4,7 @@ Color.new('gray0', '#272822')
 Color.new('gray1', '#1c1c16')
 Color.new('gray2', '#43433c')
 Color.new('white',     '#F8F8F2')
-Color.new('red',       '#f92672')
+Color.new('red',       '#ff80ae')
 Color.new('green',     '#A6E22E')
 Color.new('yellow',    '#E6DB74')
 Color.new('blue',      '#66D9EF')
@@ -15,11 +15,15 @@ Color.new('lightorange',     '#E69F66')
 
 
 Group.new('ColorColumn', c.none, c.gray2, style.none)
-Group.new('CursorLineNr', c.blue, c.none, style.bold)
 Group.new('Comment', c.grey, c.none, style.italic)
+Group.new('Visual', c.black, c.yellow, style.none)
 -- Group.new('Normal', c.none, c.none, style.none)
 -- Group.new('LineNr', c.gray3, c.none, style.none)
 -- Group.new('SignColumn', c.gray2, c.none, style.none)
+
+-- Tab Rendering
+Group.new('TabActive', c.white, c.gray1, style.bold)
+Group.new('TabInactive', c.white, c.gray2, style.none)
 
 -- LSP Diagnostic
 Group.new('LspDiagnosticsVirtualTextHint', c.white, c.gray1, style.none)
